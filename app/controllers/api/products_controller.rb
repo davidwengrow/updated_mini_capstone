8f189bdc2d333c0f80cbedf4a77cfa0455c46d9b
David Wengrow <dwengrow@gmail.com>
2019-03-03 19:45:13 -0600

    capstone segment

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,9 +1,12 @@
 class Api::ProductsController < ApplicationController
   def one_product_method
-
-
     @products = Product.all
     render "one_product.json.jbuilder"
+  end
+  def search_products_method
+    product_id = params["id"]
+    @products = Product.find_by(id: product_id)
+    render "search_products.json.jbuilder"
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # end
   namespace :api do 
     get "/one_product_url" => "products#one_product_method"
+    get "search_products_url/:id" => "products#search_products_method"
   end
 end
