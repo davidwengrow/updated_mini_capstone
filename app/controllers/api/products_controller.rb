c9bf7276af56a8528cfca4ce9d3e5ab991c0786d
David Wengrow <dwengrow@gmail.com>
2019-03-03 23:07:12 -0600

    delete shit

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -10,10 +10,24 @@ class Api::ProductsController < ApplicationController
   def create
     @product = Product.new(name: params[:name], 
       price: params[:price],
-       image_url:[:image_url],
-        description:[:description])
+       image_url: params[:image_url],
+        description: params[:description])
     @product.save
     render "show.json.jbuilder"
+  end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jbuilder"
+  end
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "Product Destroyed"} 
   end
 end
 
